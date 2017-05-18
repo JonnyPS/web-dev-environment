4a1ffa465e9ee85c4a73804b0b0cbf518205bc64
Jonny Spry <aml-jonny@AML-MAC-DT-113.local>
2017-05-18 17:09:43 +0100

    add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
+// enable gulp
 var gulp = require('gulp');
+// enable the sass compiler 
 var sass = require('gulp-sass');
+// enable browser-sync
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
   // return any scss files in the scss folder
   return gulp.src('app/scss/*.scss')
-    // run the complier task
-    .pipe(sass())
-    // output css to the css foldercss
-    .pipe(gulp.dest('app/css'))
+  // run the complier task
+  .pipe(sass())
+  // output css to the css foldercss
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
-gulp.task('watch', function() {
+
+// use browserSync to refresh the browser using the contents of the app folder
+gulp.task('browserSync', function() {
+  browserSync.init({
+        injectChanges: true,
+
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+
+// use gulp to watch files, when files are saved gulp will automatically compile the scss to css
+gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('app/scss/*.scss', ['sass']); 
+  gulp.watch('app/*.html', browserSync.reload); 
 })
